e195ad27b8276cc06e8b3af2aa3ee25c676541b5
Mithun <mselvaratnam@gmail.com>
2017-02-07 16:50:16 -0500

    le broken

--- modules/App.js
+++ modules/App.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import 'aframe';
+import 'aframe-audioanalyser-component';
+import {Entity, Scene} from 'aframe-react';
 
 const App = () => (
   <div>
+    <Scene>
+      <Entity geometry={{primitive: 'box'}} material='color: blue'/>
+    </Scene>
     <h1>looka me im a react app</h1>
     <ul role="nav">
       <li>a))))</li>
